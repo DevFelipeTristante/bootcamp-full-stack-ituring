849f325b79effefe10537e203fdf99df08bea5df
DevFelipeTristante <felipetristante123@gmail.com>
2023-03-08 17:35:15 -0300

    adicionar commit novo

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,3 +3,9 @@ const email = "felipetristante123@gmail.com";
 const profissao = "Programador";
 const idade = 19;
 const lugar = "Barcelona";
+
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(idade);
+console.log(lugar);
