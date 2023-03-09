ead61368b1a8db289f9eb34a3fde1d0d2e8c364c
DevFelipeTristante <felipetristante123@gmail.com>
2023-03-09 01:10:36 -0300

    adicionar commit ao quinto arquivo

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,8 @@
+const nome = "Felipe Gomes Tristante";
+const email = "felipetristante123@gmail.com";
+const profissao = "Programador";
+const idade = 19;
+const lugar = "Barcelona";
+
+const mensagem = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email} ? Ficamos no aguardo. Muito obrigado e boa viagem!`;
+console.log(mensagem);
