2fb2bec57de2534eb457830cba07c3501cea8ef4
DevFelipeTristante <felipetristante123@gmail.com>
2023-03-09 00:45:34 -0300

    adicionar commit ao terceiro arquivo

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,11 @@
+const nome = "Felipe Gomes Tristante";
+const email = "felipetristante123@gmail.com";
+const profissao = "Programador";
+const idade = 19;
+const lugar = "Barcelona";
+
+console.log(typeof nome === "string");
+console.log(typeof email === "string");
+console.log(typeof profissao === "string");
+console.log(typeof idade === "string");
+console.log(typeof lugar === "string");
