a06c8c7033b7f980687cc48168777e85b5c0104c
DevFelipeTristante <felipetristante123@gmail.com>
2023-03-10 00:55:53 -0300

    adicionar commit ao segundo arquivo da tarefa 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,24 @@
+function saudacao(nomeDoComprador) {
+    console.log(`Olá, ${nomeDoComprador}!\n`)
+}
+
+function mensagem(nomeDoProduto, nomeDoRecebedor, enderecoEntrega) {
+    console.log(
+        `Fizemos a entrega do produto ${nomeDoProduto} no endereço ${enderecoEntrega} com sucesso!
+        O seu produto foi recebido por: ${nomeDoRecebedor}
+        Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar.\n`
+    )
+}
+
+function agradecimento(nomeEnviante) {
+    console.log(
+        `
+        Atenciosamente,
+        ${nomeEnviante}.
+        `        
+    )
+}
+
+saudacao("Jorge")
+mensagem("Tênis", "Felipe", "Rua Centro 1000")
+agradecimento("Carol")
