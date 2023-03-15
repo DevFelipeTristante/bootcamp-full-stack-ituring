66ad8c2d73384dd532ff427407afcf71af2eaa2f
DevFelipeTristante <felipetristante123@gmail.com>
2023-03-14 23:47:17 -0300

    adicionar commit ao nono arquivo da tarefa 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-9.js
@@ -0,0 +1,11 @@
+function maior(num1,num2) {
+    if (num1>num2)
+        console.log(`O número ${num1} é maior`)
+    else
+        console.log(`O número ${num2} é maior`)
+}
+
+maior(23,30)
+maior(50,18)
+maior(22,45)
+maior(40,11)
