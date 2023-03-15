7f95fc61910746e5523edaf46991acaf1175e0c9
DevFelipeTristante <felipetristante123@gmail.com>
2023-03-14 23:56:18 -0300

    adicionar commit ao decimo arquivo da tarefa 2

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,13 @@
+function maior(num1,num2,num3) {
+    if (num1>num2 && num1>num3)
+        console.log(`O número ${num1} é maior`)
+    else if (num2>num3)
+        console.log(`O número ${num2} é maior`)
+    else
+        console.log(`O número ${num3} é maior`)
+}
+
+maior(23,30,77)
+maior(50,18,37)
+maior(22,45,21)
+maior(40,11,73)
